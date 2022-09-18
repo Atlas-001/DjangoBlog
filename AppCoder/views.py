eb0c086f9210ed36473fee092c7cb8b215e7155e
Maverick <peterjamesfpj@gmail.com>
2022-09-17 22:39:03 -0300

    Version 0.2

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+def inicio(request):
+    return render(request, "AppCoder/inicio.html")
+
+def vista(request):
+    return render(request, "AppCoder/vista.html")
