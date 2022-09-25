649a27d4ef97aa3c2df11009a2a84dc87375ca42
Maverick <peterjamesfpj@gmail.com>
2022-09-25 01:05:24 -0300

    version 0.9

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("formSerie", formuSerie, name = "FormSerie"),
     path("formPeli", formuPeli, name = "FormPeli"),
     path("formUsuario", formu, name = "FormUsuario"),
-    path("buscando", busqueda, name = "Buscando"),
     path("buscador/", buscarUsuario, name = "Buscador"),
     
 ]
